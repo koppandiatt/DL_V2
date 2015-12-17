b5398ca36fc1eab0490ec59ccc45b1aa3097992d
Attila Alin <koppandi.alin@gmail.com>
2015-12-17 11:00:37 +0200

    OnBackPress

--- app/src/main/java/DataAccessLayer/DataAccessLayer.java
+++ app/src/main/java/DataAccessLayer/DataAccessLayer.java
@@ -68,7 +68,19 @@ public class DataAccessLayer {
     }
 
 
+    public void insert(String query){
 
+        if (_conn == null) {
+            return;
+        }
+        try {
+            Statement statement = _conn.createStatement();
+            statement.executeQuery(query);
+        }catch(Exception ex){
+            Log.e("dberror","Error occured at selecting");
+        }
+        return;
+    }
 
 
     public String getUserRole(String username, String password) {
